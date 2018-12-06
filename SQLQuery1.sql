c04ecc20c2630fe98b28f5c3673e5dfa13dfd37b
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-06 18:34:30 +0530

    a
    a

---
+++ SQLQuery1.sql
@@ -0,0 +1,36 @@
+--DDL data defination language
+--create
+--alter
+--drop
+
+Create database dbHarsh;
+go
+
+Create table tblInfo
+(
+	InfoId int primary key identity,
+	--constrains
+	Name varchar(40) not null unique,
+	CityName varchar(40) not null unique,
+	Gender varchar(10) not null
+);
+create table tblCategory 
+(
+	CatId int primary key identity,
+	CatName varchar(40) not null unique
+);
+create table tblProduct
+(
+	ProductId int primary key identity,
+	ProductName varchar(50) not null,
+	MngDate date not null,
+	RefCatId int foreign key references tblCategory(CatId)
+);
+create table tblOrder
+(
+	OrderId int primary key identity,
+	RefProductId int foreign key references tblProduct(ProductId),
+	RefInfoId int foreign key references tblInfo(InfoId),
+	OrderDate date not null
+
+)
